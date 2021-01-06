fb033a042f05d40b8e1d21cdc7b895174bfd523b
unknown <kkarazle@outlook.com>
2021-01-07 00:03:07 +0800

    Finalized

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
-import {TouchableOpacity, View, Text, Linking, Button, StyleSheet, Alert} from 'react-native';
+import React, { Component, useState } from 'react';
+import {TouchableOpacity, View, Text, Button, StyleSheet } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 
 export default class App extends Component {
-  //states to be used 
-  state = {
-    isQRScanning: false,
+
+  //init states
+  constructor(props) {
+    super(props);
+    this.state = {
+      isQRScanning: false,
+      firstName: "",
+      lastName: "",
+      email: ""
+    };
   }
 
   //function to contact the backend API
@@ -23,30 +30,34 @@ export default class App extends Component {
     console.log(res)
   }).catch(function(error) {
     console.log(error.message);
-     // ADD THIS THROW error
-      throw error;
     });
   }
 
   //this function is to display the QR code data
   onceScanned = e => {
-    Alert.alert(`The QR Code returned this link / data : ${e.data}`)
-    this.setState({isQRScanning: false})
+    let url = decodeURIComponent(e.data)
+    url = JSON.parse(url.substring(url.indexOf("=") + 1))
+    this.setState({
+      isQRScanning: false,
+      firstName:url['firstName'],
+      lastName:url['lastName'],
+      email:url['email']
+    })
     this.saveData(e.data)
   }
 
   render(){
-    const { isQRScanning } = this.state
     return (
       <> 
             <View style={{padding: 30}}>            
             <Text style={styles.titleStyle}>Basic QR Scanner</Text>
-            <Text style={{textAlign: 'center'}}>Scan any barcode and the results will be shown in an alert box!</Text>
+            <Text style={{textAlign: 'center'}}>Scan any barcode and the results will be shown below!</Text>
             <TouchableOpacity style={{ height: 100, width: 100, marginLeft: 100, marginRight: 100, paddingTop: 10}}>
-            {isQRScanning ? 
+
+            {this.state.isQRScanning ? //change button state depending on action
             <Button
               onPress={()=>{this.setState({
-                isQRScanning: false
+                isQRScanning: false,
               })}}
               title="Stop!"
               color="#000"
@@ -54,17 +65,33 @@ export default class App extends Component {
               : 
             <Button
               onPress={()=>{this.setState({
-                isQRScanning: true
+                isQRScanning: true,
+                firstName: "",
+                lastName: "",
+                email: ""
               })}}
               title="Start!"
               color="#000"
             />
           }
             </TouchableOpacity>
-            {isQRScanning ? (
-            <QRCodeScanner
-            onRead={this.onceScanned}
-            ></QRCodeScanner>):null}
+              { //onRead is the action taken once QR is scanned
+              this.state.isQRScanning ? 
+              (<QRCodeScanner onRead={this.onceScanned}></QRCodeScanner>)
+              : null 
+              } 
+            
+                {
+                  //start displaying details once QR is scanned
+                  this.state.firstName && this.state.lastName && this.state.email ? (
+                   <View>
+                      <Text>First name: {this.state.firstName}</Text>
+                      <Text>Last name: {this.state.lastName}</Text>
+                      <Text>Email: {this.state.email}</Text>
+                   </View>
+                  ):null
+                }
+
             </View>
       </>
     );
